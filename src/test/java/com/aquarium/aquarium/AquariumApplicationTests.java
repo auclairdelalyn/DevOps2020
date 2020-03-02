fbfa3564c850d935270487b302c03283bca5314b
Thomas <thomas.levee@gmail.com>
2020-03-02 16:25:26 +0100

    test test

--- src/test/java/com/aquarium/aquarium/AquariumApplicationTests.java
+++ src/test/java/com/aquarium/aquarium/AquariumApplicationTests.java
@@ -1,13 +1,19 @@
 package com.aquarium.aquarium;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AquariumApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(1,1);
 	}
 
 }
